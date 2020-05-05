9e43b871c66fffce495d119c130f8e8f0db17d59
hooke-naoto <naoto40@gmail.com>
2020-05-06 00:02:02 +0900

    Modification for sh-file.

--- HektorDetection2020.sh
+++ HektorDetection2020.sh
@@ -1,5 +1,4 @@
 #!/bin/bash --rcfile
-source /home/pi/env/bin/activate
 export GOOGLE_APPLICATION_CREDENTIALS=/home/pi/GoogleApplicationCredentials.json
 echo "Hektor Detection 2020 is running!"
 python /home/pi/programs/HektorDetection2020/HektorDetection2020.py
