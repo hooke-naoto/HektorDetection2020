51c048f2bad715044b5c1f3695ca65c53749536b
hooke-naoto <naoto40@gmail.com>
2020-06-01 14:11:14 +0900

    Test sound.

--- test/20200601_test_sound.py
+++ test/20200601_test_sound.py
@@ -1,7 +1,11 @@
-import os
+import subprocess
 import glob
 import random
 dir_sound = "sound/"
 Files = sorted(glob.glob(dir_sound + "*.mp3"))
-os.system("aplay " + Files[random.randint(0, len(Files) - 1)])
-print('Files:', Files)
+# print(len(Files))
+# print(random.randint(0, len(Files) - 1))
+# print(Files[random.randint(0, len(Files) - 1)])
+while True:
+    subprocess.call("vlc -I rc --play-and-exit "  + Files[random.randint(0, len(Files) - 1)], shell=True)
+    
